98c0c1e669ebc9c4247e7a1cff40d1ed93eea95c
Carl Beaverson <carlbeaverson@gmail.com>
2023-06-19 15:09:32 +1000

    Change to starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `GC Starter Theme`,
+    description: `Groundcrew Starter Theme 2023`,
+    author: `@groundcrew`,
+    siteUrl: `https://groundcrew.com.au`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -23,20 +23,30 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `47244b367354d9df68bc03a796cf21`, 
+      }
+    },
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-scroll-reveal`,
+      options: {
+        once: true,
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `starter`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
+        background_color: `#000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`,
       },
     },
   ],
