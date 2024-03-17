649c870daf7cff6569e8dfb7a656810baa15606e
liquid-O2 <this.is.arunava.b@gmail.com>
2024-03-17 10:40:29 +0530

    Change to admin

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,6 +1,6 @@
 import { useLayoutEffect } from "react"
 
 export default function AdminPage() {
-    useLayoutEffect(() => (window.location.href = ""), [])
+    useLayoutEffect(() => (window.location.href = "/"), [])
     return null
 }
